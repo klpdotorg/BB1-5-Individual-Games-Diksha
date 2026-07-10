7459eb832b720ba32f61dd6d3b40a0046a2070d8
Arathi22 <arathirajagopalan22@gmail.com>
2026-07-10 15:07:59 +0530

    bb games

---
+++ game_unity_2_2_3a/js/index.js
@@ -0,0 +1,29 @@
+var _this;
+
+var app={
+	initialize:function(){
+		if(window.cordova){
+			document.addEventListener('deviceready',this.onDeviceReady.bind(this),false);
+			return;
+		}
+		this.receivedEvent();
+	},
+	onDeviceReady:function(){ this.receivedEvent(); },
+	receivedEvent:function(id){
+		var game=new Phaser.Game(960,540,Phaser.CANVAS,'');
+
+		game.state.add('boot',Game.boot);
+		game.state.add('preloader_unity_2_2_3a',Game.preloader_unity_2_2_3a);
+
+		// Game states
+		game.state.add('unity2_2_3alevel1', Game.unity2_2_3alevel1);
+		game.state.add('unity2_2_3aScore', Game.unity2_2_3aScore);
+
+		// Common states
+		game.state.add('score',Game.Score);
+		game.state.add('Backbutton',Game.Backbutton);
+
+		game.state.start('boot');
+	}
+};
+app.initialize();
---
+++ game_unity_2_3_1a/js/index.js
@@ -0,0 +1,29 @@
+var _this;
+
+var app={
+	initialize:function(){
+		if(window.cordova){
+			document.addEventListener('deviceready',this.onDeviceReady.bind(this),false);
+			return;
+		}
+		this.receivedEvent();
+	},
+	onDeviceReady:function(){ this.receivedEvent(); },
+	receivedEvent:function(id){
+		var game=new Phaser.Game(960,540,Phaser.CANVAS,'');
+
+		game.state.add('boot',Game.boot);
+		game.state.add('preloader_unity_2_3_1a',Game.preloader_unity_2_3_1a);
+
+		// Game states
+		game.state.add('unity2_3_1alevel1', Game.unity2_3_1alevel1);
+		game.state.add('unity2_3_1aScore', Game.unity2_3_1aScore);
+
+		// Common states
+		game.state.add('score',Game.Score);
+		game.state.add('Backbutton',Game.Backbutton);
+
+		game.state.start('boot');
+	}
+};
+app.initialize();
